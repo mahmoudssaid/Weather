d83ba1523709e7cbcc0c17b51506137d92b6d2c2
mahmoud saied <mahoudsaaid0@gmail.com>
2021-05-04 19:54:01 +0200

    remove set time

--- assets/js/internal.js
+++ assets/js/internal.js
@@ -17,14 +17,13 @@ function searchWeather() {
         displayToDay();
     })();
 }
-setTimeout(weatherT, 4000)
 
-async function weatherT() {
+(async function() {
     let toDay = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=b2e98d4f6b5c4849b98213404212904&q=cairo&days=3`);
     weatherTD = await toDay.json();
     windDir = weatherTD.current.wind_dir;
     displayToDay();
-}
+})();
 
 function displayToDay() {
     switch (windDir) {
