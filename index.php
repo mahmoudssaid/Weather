d50cb3683c9c4bf5e0113d6e157aff3a9f90b637
mahmoud saied <mahoudsaaid0@gmail.com>
2021-05-05 20:12:47 +0200

    nav responsev

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include "./header.tpl"?>
+<!-- =================================== main section ================================= -->
+<section class="main-section">
+    <div class="container">
+        <div class="search-div">
+            <input type="text" class="form-control" placeholder="Find your location..." id="search">
+            <button class="btn btn-primary">Find</button>
+        </div>
+        <div class="row items">
+        </div>
+    </div>
+</section>
+<?php include "./footer.tpl"?>
